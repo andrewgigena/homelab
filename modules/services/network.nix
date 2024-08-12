02c773cd6cdce034ca28c7be4a2ed33b9975a2b6
Andrew Gigena <mail@andrewgigena.dev>
2024-08-12 17:54:17 -0300

    Fix Typo

--- modules/services/network.nix
+++ modules/services/network.nix
@@ -2,16 +2,15 @@
 
 {
   services.avahi = {
+    enable = true;
+    nssmdns4 = true;
+    publish = {
       enable = true;
-      nssmdns4 = true;
-      publish = {
-        enable = true;
-        addresses = true;
-        domain = true;
-        hinfo = true;
-        userServices = true;
-        workstation = true;
-      };
+      addresses = true;
+      domain = true;
+      hinfo = true;
+      userServices = true;
+      workstation = true;
     };
   };
 }
