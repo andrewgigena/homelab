33674760f9f320aa1eb7f9678b52c075f103f118
Andrew Gigena <mail@andrewgigena.dev>
2024-08-12 17:52:50 -0300

    Splitted services into files

--- modules/services/default.nix
+++ modules/services/default.nix
@@ -2,6 +2,8 @@
 
 {
   imports = [
+    ./audio.nix
+    ./network.nix
     ./syncthing.nix
     ./virtualisation.nix
   ];
@@ -12,29 +14,6 @@
     libinput.enable = true;
     openssh.enable = true;
     flatpak.enable = true;
-
-    pipewire = {
-      enable = true;
-      alsa = {
-        enable = true;
-        support32Bit = true;
-      };
-      pulse.enable = true;
-      jack.enable = true;
-    };
-
-    avahi = {
-      enable = true;
-      nssmdns4 = true;
-      publish = {
-        enable = true;
-        addresses = true;
-        domain = true;
-        hinfo = true;
-        userServices = true;
-        workstation = true;
-      };
-    };
   };
 
   programs.kdeconnect.enable = true;
