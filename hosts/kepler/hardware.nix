2676d08fbb989e4379965b51b9b56825270f4071
Andrew Gigena <mail@andrewgigena.dev>
2024-11-17 09:30:35 -0300

    Kepler: Add mount directories for disks

--- hosts/kepler/hardware.nix
+++ hosts/kepler/hardware.nix
@@ -52,6 +52,28 @@
     options = [ "fmask=0022" "dmask=0022" ];
   };
 
+  # TODO: Encrypt extra drives
+  # Frodo - Samsung SSD 860 EVO 500GB
+  fileSystems."/disks/frodo" = {
+    device = "/dev/disk/by-uuid/ee9b5975-4368-4960-bcbb-1a30bff4208d";
+    fsType = "btrfs";
+    options = [ "compress=zstd" "noatime" "nofail" "x-systemd.device-timeout=5"];
+  };
+
+  # Gandalf - FUJITSU MJA2320BH G2
+  fileSystems."/disks/gandalf" = {
+    device = "/dev/disk/by-uuid/75009ea3-cb1f-4cc0-ac88-e403e44e232a";
+    fsType = "btrfs";
+    options = [ "compress=zstd" "noatime" "nofail" "x-systemd.device-timeout=5"];
+  };
+
+  # Legolas - WDC WD10JPVX-60JC3T0
+  fileSystems."/disks/legolas" = {
+    device = "/dev/disk/by-uuid/0090e30f-007a-45ea-a23f-6868a371a41d";
+    fsType = "btrfs";
+    options = [ "compress=zstd" "noatime" "nofail" "x-systemd.device-timeout=5"];
+  };
+
   swapDevices = [ ];
 
   networking.useDHCP = lib.mkDefault true;
