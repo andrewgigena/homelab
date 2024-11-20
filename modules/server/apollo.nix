b8a9bb13c594c2067bf81b4451d6cfc4be885da8
Andrew Gigena <mail@andrewgigena.dev>
2024-11-20 03:12:39 -0300

    Apollo: Enable remote internet access and more services

--- modules/server/apollo.nix
+++ modules/server/apollo.nix
@@ -8,10 +8,6 @@
     configDir = "/disks/nas/configs/syncthing";
   };
 
-  environment.systemPackages = [
-    pkgs.owntracks-recorder
-  ];
-
   virtualisation.docker.enable = true;
   virtualisation.oci-containers.backend = "docker";
   virtualisation.oci-containers.containers = {
@@ -19,13 +15,17 @@
       image = "jc21/nginx-proxy-manager:latest";
       autoStart = true;
       ports = [
-        "8080:80"
-        "8081:81"
-        "8443:443"
+        "80:80"
+        "81:81"
+        "443:443"
       ];
       volumes = [
         "/disks/nas/configs/nginx/data:/data"
         "/disks/nas/configs/nginx/letsencrypt:/etc/letsencrypt"
+      ];
+
+      extraOptions = [
+        "--network=apollo"
       ];
     };
 
@@ -37,7 +37,7 @@
         "53:53/tcp"
         "53:53/udp"
         "67:67/udp"
-        "80:80/tcp"
+        "8000:80/tcp"
       ];
 
       volumes = [
@@ -52,7 +52,7 @@
 
       extraOptions = [
         "--cap-add=NET_ADMIN"
-        "--network=bridge"
+        "--network=apollo"
       ];
     };
 
@@ -65,16 +65,42 @@
       ];
 
       volumes = [
-        "/disks/nas/configs/ddns-updater/config.json:/updater/data/config.json"
+        "/disks/nas/configs/ddns-updater:/updater/data"
       ];
 
+
       extraOptions = [
-        "--network=bridge"
+        "--network=apollo"
       ];
     };
 
-    # TODO: Setup the following containers instead of using native ones:
-    # - Owntracks
-    # - Syncthing
+    owntracks-recorder = {
+      image = "owntracks/recorder";
+      ports = [
+        "8083:8083"
+      ];
+      environment = {
+        OTR_PORT = "0"; # Disable MQTT by setting the environment variable.
+      };
+      volumes = [
+        "/disks/nas/configs/owntracks:/config"
+        "/disks/nas/data/owntracks:/store"
+      ];
+      autoStart = true;
+      extraOptions = [
+      	"--name=owntracks-recorder"
+	"--network=apollo"
+      ];
+    };
+  };
+
+  systemd.services."docker-network-apollo" = {
+    description = "Create Docker Network Apollo";
+    serviceConfig.Type = "oneshot";
+    wantedBy = [ "multi-user.target" ];
+    script = ''
+      ${pkgs.docker}/bin/docker network inspect apollo > /dev/null 2>&1 || \
+      ${pkgs.docker}/bin/docker network create apollo --subnet 172.20.0.0/16
+    '';
   };
 }
